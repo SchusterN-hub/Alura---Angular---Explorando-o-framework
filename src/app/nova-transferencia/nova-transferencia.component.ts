20f400c6195834e279115caed52cd4973952a4df
Nicolas Schuster <ncschuster@gmail.com>
2022-06-29 17:49:05 -0300

    Curso finalizado

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,32 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  public valor: number;
+  public destino: number;
+
+  constructor(private service: TransferenciaService, private router: Router){ }
+  transferir() {
+    console.log('transferi');
+    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado)
+      this.limparCampos();
+      this.router.navigateByUrl('extrato');
+    },
+      (error) => console.error(error)
+    )
+  }
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
